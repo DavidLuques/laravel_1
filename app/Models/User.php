4d54fb874cc22e4cf3cc9fc294d6b3d35c119eb0
DavidLuques <braian.luques2@gmail.com>
2023-06-06 19:59:54 -0300

    v17 ongoing

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Support\Str;
+
+
+
 
 class User extends Authenticatable
 {
@@ -42,4 +47,12 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    protected function name(): Attribute {
+        return new Attribute(
+            get:  fn($value)=>Str::title($value),
+                    //accesor   
+            set: fn($value)=>  mb_strtolower($value,'UTF-8')
+                                                         // return str::strtolower($value);        
+        );
+    }
 }
